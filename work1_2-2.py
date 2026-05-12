6ad8ccbfb7138a8c0cd81821dca8ce776aeeaa07
krinal dholakiya <krinaldholakiya@gmail.com>
2026-05-12 18:40:15 +0530

    work

---
+++ work1_2-2.py
@@ -0,0 +1,13 @@
+a=input("enter your name:")
+b=input("enter your age:")
+c=input("enter you hobby:")
+print('"',"Hello,",a,"!at",b,",enjoying",c,"sounds fun!",'"')
+
+'''
+output:
+enter your name:mahek
+enter your age:14
+enter you hobby:dance
+" Hello, mahek !at 14 ,enjoying dance sounds fun! "
+
+'''
