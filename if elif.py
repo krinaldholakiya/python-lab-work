8522e2687c89542a9dafd00105b187c0402e11da
krinaldholakiya <krinaldholakiya@gmail.com>
2026-05-17 10:38:38 +0530

    Add files via upload

---
+++ if elif.py
@@ -0,0 +1,9 @@
+a=int(input("enter your age:"))
+if a<=12:
+    print("child")
+elif a>=13 and a<=19:
+    print("teenager")
+elif a>=20 and a<=59:
+    print("adult")
+elif a>=60:
+    print("senior")
